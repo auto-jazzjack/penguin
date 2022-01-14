f3aec48a9619e215c536c8f351261e9a95c601e5
kwon <jazzjack@kwonui-MacBookPro.local>
2022-01-15 02:11:55 +0900

    Fix

--- stateful/redis.sh
+++ stateful/redis.sh
@@ -7,7 +7,7 @@ echo daemonize yes                    >> ../redis-6.2.6/redis.conf
 echo cluster-enabled yes              >> ../redis-6.2.6/redis.conf
 echo appendonly no                    >> ../redis-6.2.6/redis.conf
 echo save \"\"                          >> ../redis-6.2.6/redis.conf
-echo pidfile $SHELL_PATH/redis-6.2.6/nodes.conf  >> ../redis-6.2.6/redis.conf
+echo pidfile $SHELL_PATH/redis-6.2.6/cluster-config-file  >> ../redis-6.2.6/redis.conf
 
 
 ../redis-6.2.6/redis-server ../redis-6.2.6/redis.conf
