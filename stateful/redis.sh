01749a10976645dc7c970f63dca53fc9cb9b687b
kwon <jazzjack@kwonui-MacBookPro.local>
2022-01-15 02:41:00 +0900

    fix path

--- stateful/redis.sh
+++ stateful/redis.sh
@@ -1,13 +1,14 @@
+pkill redis-server && sleep 2
+
 touch ../redis-6.2.6/redis.conf
 
-SHELL_PATH=`pwd -P`
+SHELL_PATH=`cd .. && pwd -P`
 
 echo port 6379                        > ../redis-6.2.6/redis.conf
 echo daemonize yes                    >> ../redis-6.2.6/redis.conf
 echo cluster-enabled yes              >> ../redis-6.2.6/redis.conf
 echo appendonly no                    >> ../redis-6.2.6/redis.conf
 echo save \"\"                          >> ../redis-6.2.6/redis.conf
-echo pidfile $SHELL_PATH/redis-6.2.6/cluster-config-file  >> ../redis-6.2.6/redis.conf
+echo "cluster-config-file $SHELL_PATH/redis-6.2.6/nodes.conf"  >> ../redis-6.2.6/redis.conf
 
-
-../redis-6.2.6/redis-server ../redis-6.2.6/redis.conf
+exec $SHELL_PATH/redis-6.2.6/redis-server $SHELL_PATH/redis-6.2.6/redis.conf
