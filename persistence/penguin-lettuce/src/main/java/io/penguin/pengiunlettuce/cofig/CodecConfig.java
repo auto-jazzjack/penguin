5c5f511acce2d60969756e6f7c90025ee6c1b211
kwon <jazzjack@kwonui-MacBookPro.local>
2022-01-30 00:23:42 +0900

    fix

--- persistence/penguin-lettuce/src/main/java/io/penguin/pengiunlettuce/cofig/CodecConfig.java
+++ persistence/penguin-lettuce/src/main/java/io/penguin/pengiunlettuce/cofig/CodecConfig.java
@@ -1,11 +1,15 @@
 package io.penguin.pengiunlettuce.cofig;
 
 import io.penguin.penguincodec.Codec;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CodecConfig {
     private Class<Codec> codec;
 }
