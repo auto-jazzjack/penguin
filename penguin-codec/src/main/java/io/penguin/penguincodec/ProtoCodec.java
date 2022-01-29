b14197bdc4828cad3a97c7718a66bb7f99e9785a
kwon <jazzjack@kwonui-MacBookPro.local>
2022-01-29 21:03:27 +0900

    fix codec

--- penguin-codec/src/main/java/io/penguin/penguincodec/ProtoCodec.java
+++ penguin-codec/src/main/java/io/penguin/penguincodec/ProtoCodec.java
@@ -3,12 +3,20 @@ package io.penguin.penguincodec;
 import com.google.protobuf.Message;
 import com.google.protobuf.Parser;
 
+import java.lang.reflect.Method;
+
 public class ProtoCodec<V extends Message> implements Codec<V> {
 
     private final Parser<? extends V> parserForType;
 
     public ProtoCodec(Class<V> clazz) throws Exception {
-        parserForType = (Parser<? extends V>) clazz.getDeclaredMethod("getParserForType").invoke(null);
+
+        Method newBuilder = clazz.getDeclaredMethod("newBuilder");
+        newBuilder.setAccessible(true);
+        Message.Builder builder = (Message.Builder) newBuilder.invoke(clazz);
+
+        parserForType = (Parser<? extends V>) clazz.getDeclaredMethod("getParserForType")
+                .invoke(builder.build());
     }
 
     @Override
