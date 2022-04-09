f94ab204dff86c0006a307b2d9287d68f46c529f
kwon <jazzjack@kwonui-MacBookPro.local>
2022-04-09 21:39:27 +0900

    fx

--- penguin-core/src/main/java/io/penguin/penguincore/compress/GZIPCompressor.java
+++ penguin-core/src/main/java/io/penguin/penguincore/compress/GZIPCompressor.java
@@ -5,17 +5,10 @@ import io.penguin.penguincore.util.IOUtils;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.util.zip.DeflaterOutputStream;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class GZIPCompressor implements Compressor {
-
-    public byte[] compress(byte[] input) throws Exception {
-
-        throw new UnsupportedOperationException(this.getClass().getName() + " is not implemented");
-    }
 
     public OutputStream compress(InputStream inputStream) throws Exception {
 
@@ -25,5 +18,15 @@ public class GZIPCompressor implements Compressor {
         }
         return outputStream;
     }
+
+    @Override
+    public OutputStream decompress(InputStream inputStream) throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(inputStream.available() * 2);
+        try (InputStream compressor = new GZIPInputStream(inputStream)) {
+            IOUtils.copy(compressor, outputStream, IOUtils.DEFAULT_SIZE);
+        }
+        return outputStream;
+    }
+
 
 }
