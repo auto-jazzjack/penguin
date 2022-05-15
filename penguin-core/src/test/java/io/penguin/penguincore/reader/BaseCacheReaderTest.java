734d7b9b8df729d092977e608ed9cdf723213882
kwon <jazzjack@kwonui-MacBookPro.local>
2022-05-15 20:53:16 +0900

    fx

--- penguin-core/src/test/java/io/penguin/penguincore/reader/BaseCacheReaderTest.java
+++ penguin-core/src/test/java/io/penguin/penguincore/reader/BaseCacheReaderTest.java
@@ -29,12 +29,13 @@ public class BaseCacheReaderTest {
     }
 
     @Test
-    public void asd22() throws Exception {
+    public void read() throws Exception {
         File file1 = new File("/Users/jazzjack/Desktop/he/asdasdasd.txt");
         FileInputStream fileInputStream = new FileInputStream(file1);
 
-        ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
-        byte[] bytes = zipInputStream.readAllBytes();
+        String s = new String(fileInputStream.readAllBytes());
+        byte[] s1 = s.replace(" ", "").replace("\n", "").getBytes();
+        byte[] bytes = convertByteToHexa(s1);
 
         File file = new File("/Users/jazzjack/Desktop/he/penguin_con222.zip");
 
@@ -63,23 +64,28 @@ public class BaseCacheReaderTest {
         FileInputStream fileInputStreamasd = new FileInputStream(file1asd);
         byte[] bytes1 = fileInputStreamasd.readAllBytes();
         byte[] bytes2 = convertByteToHexa(bytes1);
-        System.out.println();
+
+        File file2 = new File("/Users/jazzjack/Desktop/he/result.txt");
+        FileOutputStream fileOutputStream1 = new FileOutputStream(file2);
+        fileOutputStream1.write(bytes2);
+        //System.out.println();
 
         /*String s = new String(bytes);
         System.out.println();*/
 
     }
 
+    //Read after convert
     public static byte[] convertByteToHexa(byte[] byteArray) {
         byte[] hex = new byte[byteArray.length / 2];
 
         // Iterating through each byte in the array
         for (int i = 0; i < byteArray.length; i += 2) {
 
-            byte left = byteArray[i];
-            byte right = byteArray[i + 1];
+            byte left = nomalize(byteArray[i]);
+            byte right = nomalize(byteArray[i + 1]);
 
-            hex[i / 2] = (byte) (left + ((byte) (right * 16)));
+            hex[i / 2] = (byte) (right + ((byte) (left * 16)));
             //hex[i / 2] -= '0';
             //hex[i / 2] -= '0';
 
@@ -88,14 +94,15 @@ public class BaseCacheReaderTest {
         return hex;
     }
 
-    public byte nomalize(byte b) {
+    public static byte nomalize(byte b) {
         if (b >= 'a' && b <= 'z') {
-            return (byte) (b - 'a');
+            return (byte) (b - 'a' + 10);
         } else if (b >= '0' && b <= '9') {
             return (byte) (b - '0');
-        } else if (b >= 'a' && b <= 'z') {
-            return (byte) (b - 'a');
+        } else if (b >= 'A' && b <= 'Z') {
+            return (byte) (b - 'A');
         }
+        return b;
     }
 
     public static String convertByteToHexadecimal(byte[] byteArray) {
