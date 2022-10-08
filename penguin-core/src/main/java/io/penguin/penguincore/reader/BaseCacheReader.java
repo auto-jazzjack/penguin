38001f70d83978b8c53f2ed07ee1a0abd3b80eb8
kwon <jazzjack@kwonui-MacBookPro.local>
2022-10-08 22:10:29 +0900

    fx

--- penguin-core/src/main/java/io/penguin/penguincore/reader/BaseCacheReader.java
+++ penguin-core/src/main/java/io/penguin/penguincore/reader/BaseCacheReader.java
@@ -4,8 +4,4 @@ import io.penguin.penguincore.writer.Writer;
 
 public interface BaseCacheReader<K, V> extends Reader<K, CacheContext<V>>, Writer<K, CacheContext<V>> {
 
-    String SOURCE_CACHE_REFRESH_LATENCY = "source_refresh_cache_latency";
-    String SOURCE_CACHE_REFRESH_COUNT = "source_refresh_cache_count";
-
-
 }
